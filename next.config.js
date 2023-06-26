1aa5081799a2a36368b670b8c58b65668dfad26a
Maikel33 <perezguedesmaikel@gmail.com>
2023-06-26 17:43:36 -0400

    module installation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const withFonts = require('next-fonts');
-module.exports = withFonts();
+// module.exports = withFonts();
+module.exports = {
+    // ...
+    i18n: {
+        locales: ['en', 'es'],
+        defaultLocale: 'en',
+    },
+    // ...
+};
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
