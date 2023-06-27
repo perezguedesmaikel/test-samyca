facb6d5e077264653ac460efde2052b0835e2a78
Maikel33 <perezguedesmaikel@gmail.com>
2023-06-27 00:02:10 -0400

    flag language ready

--- components/ChangeLanguaje.tsx
+++ components/ChangeLanguaje.tsx
@@ -5,17 +5,18 @@ import {useState} from "react";
 
 const ChangeLanguage = () => {
     const router = useRouter();
+    const {locale} = router;
     const [language, setLanguage] = useState<'en' | 'es'>('es');
 
     const changeLanguage = () => {
-        setLanguage(language === 'es' ? 'en' : 'es');
-        router.push(router.pathname, router.asPath, {locale: language});
+        setLanguage(locale === 'en' ? 'es' : 'en');
+        router.push(router.pathname, router.asPath, {locale: locale === 'en' ? 'es' : 'en'});
     };
 
     return (
         <Image
             className='cursor-pointer'
-            src={language === 'es' ? '/Group 28.svg' : "/bandera.svg"}
+            src={locale === 'es' ? '/Group 28.svg' : "/bandera.svg"}
             alt="English"
             width={27}
             height={18}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,15 @@ import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import Layout from "../components/Layout";
 import {NextIntlProvider} from "next-intl";
+import {useRouter} from 'next/router';
+import enMessages from '../locales/en.json';
+import esMessages from '../locales/es.json';
 
 function MyApp({Component, pageProps}: AppProps) {
-    return <NextIntlProvider messages={pageProps.messages}> <Layout>
+    const router = useRouter();
+    const {locale} = router;
+    const messages = locale === 'es' ? esMessages : enMessages;
+    return <NextIntlProvider locale={locale} messages={messages}> <Layout>
         <div className='p-40'>
             <Component {...pageProps} />
         </div>
