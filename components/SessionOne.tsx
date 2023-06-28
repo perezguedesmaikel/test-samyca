6797397266d55f7a152f18ba3a4fcc67fd107086
Maikel33 <perezguedesmaikel@gmail.com>
2023-06-27 20:01:53 -0400

    work office completed

--- components/SessionOne.tsx
+++ components/SessionOne.tsx
@@ -9,46 +9,54 @@ export default function SessionOne() {
     const router = useRouter();
     const t = useTranslations('nav');
     return (
-        <header className="bg-[#001853] h-[722px] pt-[34px] px-[56px]">
+        <>
+            <header className="bg-[#001853] h-[722px] pt-[34px] px-[56px]">
 
-            <nav className="flex items-center justify-between bg-[#FFFFFF] h-[86px] rounded-[8px] px-10">
-                <ul className="flex w-[80%] px-10">
-                    <li className={classNames('flex-grow leading-6 ', {
-                        'text-[#2E53A3] font-bold': router.pathname === '/',
-                    })}>
-                        <Link href="/">{t('start')}</Link>
-                    </li>
-                    <li className={classNames('flex-grow leading-6 ', {
-                        'text-[#2E53A3] font-bold': router.pathname === '/servicios',
-                    })}>
-                        <Link href="/servicios">{t('services')}</Link>
-                    </li>
-                    <li className={classNames('flex-grow leading-6 ', {
-                        'text-[#2E53A3] font-bold': router.pathname === '/nosotros',
-                    })}>
-                        <Link href="/nosotros">{t('us')}</Link>
-                    </li>
-                    <li className={classNames('flex-grow leading-6 ', {
-                        'text-[#2E53A3] font-bold': router.pathname === '/herramientas',
-                    })}>
-                        <Link href="/herramientas">{t('tools')}</Link>
-                    </li>
-                    <li className={classNames('flex-grow leading-6 ', {
-                        'text-[#2E53A3] font-bold': router.pathname === '/socios',
-                    })}>
-                        <Link href="/socios">{t('partners')}</Link>
-                    </li>
-                </ul>
-                <div className='flex justify-around w-[20%]'>
-                    <Link href="/contactenos"
-                          className="flex justify-center items-center bg-[#2D509E] text-white py-2 px-4 rounded-[56px] w-[175px] h-[49px] shadow-md shadow-[#709DDA] text-lg font-semibold leading-6 tracking-wide hover:bg-[#001853]">
-                        Contactar
-                    </Link>
-                    <ChangeLanguage/>
-                </div>
+                <nav className="flex items-center justify-between bg-[#FFFFFF] h-[86px] rounded-[8px] px-10">
+                    <ul className="flex w-[80%] px-10">
+                        <li className={classNames('flex-grow leading-6 ', {
+                            'text-[#2E53A3] font-bold': router.pathname === '/',
+                        })}>
+                            <Link href="/">{t('start')}</Link>
+                        </li>
+                        <li className={classNames('flex-grow leading-6 ', {
+                            'text-[#2E53A3] font-bold': router.pathname === '/servicios',
+                        })}>
+                            <Link href="/servicios">{t('services')}</Link>
+                        </li>
+                        <li className={classNames('flex-grow leading-6 ', {
+                            'text-[#2E53A3] font-bold': router.pathname === '/nosotros',
+                        })}>
+                            <Link href="/nosotros">{t('us')}</Link>
+                        </li>
+                        <li className={classNames('flex-grow leading-6 ', {
+                            'text-[#2E53A3] font-bold': router.pathname === '/herramientas',
+                        })}>
+                            <Link href="/herramientas">{t('tools')}</Link>
+                        </li>
+                        <li className={classNames('flex-grow leading-6 ', {
+                            'text-[#2E53A3] font-bold': router.pathname === '/socios',
+                        })}>
+                            <Link href="/socios">{t('partners')}</Link>
+                        </li>
+                    </ul>
+                    <div className='flex justify-around w-[20%]'>
+                        <Link href="/contactenos"
+                              className="flex justify-center items-center bg-[#2D509E] text-white py-2 px-4 rounded-[56px] w-[175px] h-[49px] shadow-md shadow-[#709DDA] text-lg font-semibold leading-6 tracking-wide hover:bg-[#001853]">
+                            Contactar
+                        </Link>
+                        <ChangeLanguage/>
+                    </div>
 
-            </nav>
+                </nav>
 
-        </header>
+
+            </header>
+            <div className='flex fixed top-[120px] right-0 w-809 h-658 z-10'>
+                {/*<div className='w-[50%] h-[500px]'>hola!!!</div>*/}
+                <Image src={'/Group 26 (1) 1.svg'} alt={'office'} width={809} height={658}
+                       style={{position: 'relative', top: '50px', right: 0, zIndex: 999}}/>
+            </div>
+        </>
     );
 }
